632f694f0a8defe438622111658b341d648588f3
kalebBd <ka.dori.kb@gmail.com>
2018-04-12 21:18:15 +0300

    updated fractale to realistic fractale and added blender simulation

--- src/ros_assignment/nodes/blender.py
+++ src/ros_assignment/nodes/blender.py
@@ -1,59 +1,42 @@
-#!/usr/bin/env python
-import rospy
-from geometry_msgs.msg import Twist
+import socket
 import bpy
+import sys
 
-#Global variables
-vel_msg = Twist()
-speed = 8
-obj = bpy.data.objects["Cube"]
+def motionreader():
+    global x,y,teta,msg
+    x = obj.location.x
+    y = obj.location.y
+    teta = obj.location.z #rotation
+    msg = str(x) + ' ' + str(y) + ' ' + str(teta)
+    print(msg)
 
-#Publisher
-def publisher(distance,vel_msg):
-    # Starts a new node
-    rospy.init_node('robot_cleaner', anonymous=True)
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    #Setting the current time for distance calculus
-    t0 = rospy.Time.now().to_sec()
-    current_distance = 0
-    while(current_distance < distance):
-        #Publish the velocity
-        velocity_publisher.publish(vel_msg)
-        #Takes actual time to velocity calculus
-        t1=rospy.Time.now().to_sec()
-        #Calculates distancePoseStamped
-        current_distance= speed*(t1-t0)
-    #After the loop, stops the robot
-    vel_msg.linear.x = 0
-    vel_msg.angular.z = 0
-    #Force the robot to stop
-    velocity_publisher.publish(vel_msg)
+def init():
+    #Global variables
+    global x,y,teta,s,obj,msg
+    x = 5.5
+    y=5.5
+    teta=0
+    msg = '0 0 0'
+    obj = bpy.data.objects["Cube"]
 
-# mobility function
-def mobilize():
-    #get the current position and angle of object(Inital)
-    x0 = obj.location.x  #get value of blender object's x position
-    z0 = obj.location.z  #get value of blender object's z position
-    #delay few seconds
-    time.sleep(0.1)
-    #get the position and rotation of object now (Final)
-    x1 = obj.location.x  #get value of blender object's new x position
-    z1 = obj.location.z  #get value of blender object's new z position
-    #subtract Intial position from Final position
-    x = x1 - x0 #get change in position of x-axis
-    z = z1 - z0 #get change in position of z-axis
-    #calcuate distance and angle change with direction
-    distance =  x
-    z = z*2*3.14/360    #change radian to degree
-    #allocate the vel_msg value properly(Update vel_msg)
-    vel_msg.linear.x = x
-    vel_msg.angular.z = z
-    #publish the result to topic
-    publisher(distance,vel_msg)
+    # create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Main function
-if __name__ == '__main__':
-    try:
-        while not rospy.is_shutdown():
-            mobilize()
-    except rospy.ROSInterruptException: pass
+    # get local machine name
+    host = socket.gethostname()
+
+    port = 12345
+
+    # connection to hostname on the port.
+    s.connect((host, port))
+
+    # Receive no more than 1024 bytes
+    #msg = s.recv(1024)
+
+
+if __name__ == "__main__":
+    init()
+    motionreader()
+    s.send(msg.encode('ascii'))
+    print('hure')
+
--- src/ros_assignment/nodes/fractale.py
+++ src/ros_assignment/nodes/fractale.py
@@ -41,27 +41,30 @@ def fractale2(angle,f2):
         fractale2(angle - 1,f2)
 
 #combination of Hexagonal and Triangular fractals with depth argument
-def fractale3(depth,f1,f2):
+def fractale3(depth,fr):
     #TODO here
     if depth > 0:
-        fractale1(16,f1)
-        fractale2(6,f2)
-        fractale3(depth - 1,f1,f2)
+        fractale1(16,fr)
+        fractale2(6,fr)
+        fractale3(depth - 1,fr)
 
 #recursive depth( True fractale)
-def fractale(loc,size,split):
-    if loc > 0 :
-        fractale3(10,size,size)
-        #turtle1_teleport_absolute(7-(loc*2),7-(loc*2),0)
-        if split == 1:
-            turtle1_teleport_relative(-loc,0.5)
-        else:
-            turtle1_teleport_relative(loc*2,0.5)
-        fractale(loc - 1, size-1,0)
-        fractale(loc, size,1)
+def fractale(loc,size):
+    if loc >= 1 :
+        fractale3(10-(3-size),size)
+        stack.append(loc)#push
+        turtle1_teleport_relative(-loc,0.5)
+        fractale(loc - 1, size-1)
+        stack.pop()#pop
+        stack.append(loc)#push
+        turtle1_teleport_relative(loc,1)
+        fractale(loc - 1, size-1)
+        stack.pop()#pop
+
 #Intitalizer function
 def init():
-    global turtle1_set_pen,turtle1_teleport_absolute,turtle1_teleport_relative,clear_background
+    global stack,turtle1_set_pen,turtle1_teleport_absolute,turtle1_teleport_relative,clear_background
+    stack = []
     rospy.wait_for_service('turtle1/set_pen')
     rospy.wait_for_service('clear')
     turtle1_set_pen = rospy.ServiceProxy('turtle1/set_pen', SetPen)
@@ -77,5 +80,5 @@ if __name__ == '__main__':
     try:
         message()
         init()
-        fractale(4,3,1)
+        fractale(4,3)
     except rospy.ROSInterruptException: pass
