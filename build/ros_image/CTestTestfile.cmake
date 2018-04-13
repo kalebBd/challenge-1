fd3dade69fdc36da764ff608084649657e6a8c88
kalebBd <ka.dori.kb@gmail.com>
2018-04-13 15:36:43 +0300

    new files for camera capture

---
+++ build/ros_image/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kaleb/git/rosworkspace/src/ros_image
+# Build directory: /home/kaleb/git/rosworkspace/build/ros_image
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
